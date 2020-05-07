1453ec3002a5207482a4abe9ebb8aaaf41448f3f
Vasco Matos <vascomato@gmail.com>
2020-05-07 18:40:13 +0100

    lab done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require('./models/Recipe');
 // Import of the data from './data.json'
 const data = require('./data');
 
@@ -14,14 +14,53 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(self => {
+
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+
+  
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    
+    return Recipe.create({
+      //object with first recipie
+      title: 'teste',
+      cuisine: 'Author'
+    });
   })
-  .catch(error => {
+  
+
+  .then(() => {
+    console.log('Iteration 3 - Insert multiple recipes: ');
+
+    return Recipe.insertMany(data);       
+  })
+
+  .then(recipe => {
+    console.log(recipe[0].title)    
+  })
+
+  .then(() => {
+    console.log('Update recipe => The Rigatoni alla Genovese');
+
+    return Recipe.findOneAndUpdate({title:'The Rigatoni alla Genove'}, {duration: 100});    
+  })
+
+  .then(() => {
+    console.log('Remove a recipe => The Carrot Cake');
+
+    return Recipe.deleteOne({title: 'Carrot Cake'});    
+  })
+
+  .then(() => {
+    console.log('Out the database:');
+
+    return mongoose.disconnect();
+  })
+
+  .catch((error) => {
     console.error('Error connecting to the database', error);
   });
